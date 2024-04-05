910fbecd2a95510d007873f4f45c94cc1ef2cb02
aintkam <kstuckey817@gmail.com>
2024-04-05 00:31:24 -0500

    ActivityLog File, Webcam Changes, Chessboard Fix
    Added an activity log for logging what you did after each coding session (as a way to explain what you did without telling everyone immediately after)
    
    Made the webcam fill the window in full screen mode and made the code neater
    
    Added a secret to the GUI file :D

---
+++ ActivityLog.md
@@ -0,0 +1,8 @@
+# I'm going to keep a log here of everything I've done along with a signature (in case if you guys want to do the same)
+
+# 4/5/24
+
+# Worked on the code for the webcam
+# Fixed the images not loading for the chessboard 
+# Added a secret to the GUI file
+# - Kam
--- Config.py
+++ Config.py
@@ -4,15 +4,19 @@ import cv2
 class WebCam(object):
 
     #By default, the resolution is 720p (1280 x 720) and the source is 0 (default webcam)
-    def __init__(self, exitKey, width=1280, height=720, source=0) -> None:
-        self.width = width
-        self.height = height
-        self.source = source
+    def __init__(self, exitKey, source=0) -> None:
         self.exitKey = exitKey
-
+        self.source = source
+        
         #Sets the capture device
         self.capture = cv2.VideoCapture(self.source)
+        self.width = int(self.capture.get(3))
+        self.height = int(self.capture.get(4))
 
+        #Allows the webcam to fullscreen and fill the window
+        cv2.namedWindow("Webcam", cv2.WND_PROP_FULLSCREEN)
+
+    #Returns if the camera is on and the curernt frame
     def updateSource(self):
         ret, frame = self.capture.read()
         return ret, frame
@@ -20,11 +24,13 @@ class WebCam(object):
     def showSource(self):
         while True:
             ret, frame = self.updateSource()
-            cv2.imshow("Webcam", frame)
-        
-            #When the user presses the exitKey, webcam turns off
-            if cv2.waitKey(1) & 0xFF == ord(self.exitKey):
-                break
+            if ret:
+
+                cv2.imshow("Webcam", frame)
+            
+                #When the user presses the exitKey, webcam turns off
+                if cv2.waitKey(1) & 0xFF == ord(self.exitKey):
+                    break
         
         #After loop is done, releases capture and closes all windows
         self.capture.release()
@@ -32,13 +38,10 @@ class WebCam(object):
 
 #Creates a class for detectig a given object using the webcam
 class DetectObject(WebCam):
-    def __init__(self, givenObject, exitKey, width=1280, height=720, source=0) -> None:
+    def __init__(self, givenObject, exitKey, source=0) -> None:
         self.givenObject = givenObject
-        super().__init__(exitKey, width, height, source)
+        super().__init__(exitKey, source)
 
 if __name__ == '__main__':
     webcam = WebCam("q")
     webcam.showSource()
-
-
-
